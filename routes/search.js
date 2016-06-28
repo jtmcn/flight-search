11c75b10ff586e4a587a1542ae4a49ddd2bfc964
Joel M <joel@mariachi.fiesta>
2016-06-27 19:23:16 -0700

    rename flights to search

--- routes/search.js
+++ routes/search.js
@@ -1,11 +1,11 @@
 'use strict';
 
+var flights = require('../common/scraper.js');
+
 module.exports = function(server) {
     server.get('/flights/search', function(req, res, next) {
         console.log('get flights');
-
-        var flightData = [];
-
+        var flightData = flights.sorted();
         res.send(flightData);
     });
 
--- test/search.spec.js
+++ test/search.spec.js
@@ -3,27 +3,29 @@
 var expect = require('chai').expect;
 var restify = require('restify');
 
-
-// init the test client
-var client = restify.createJsonClient({
-    url: 'http://localhost:8000'
-});
-
+var client = restify.createJsonClient('http://localhost:8000');
 
 describe('Endpoints', function() {
 
     describe('/flights/search', function() {
-        it('should return a 200 response', function(done) {
+        it('should return valid json data', function(done) {
             client.get('/flights/search', function(err, req, res) {
                 expect(res.statusCode).to.equal(200);
+                console.log(res.body);
+                expect(res.body).to.have.length.above(0);
                 done();
             });
         });
     });
-
+    console.log('describe');
     describe('/bananas', function() {
+        console.log('it');
         it('should return 404', function() {
+            console.log('get');
             client.get('/bananas', function(err, req, res) {
+                console.log('res head');
+                console.log(res.head);
+                // expect(res);
                 expect(res.statusCode).to.equal(404);
                 done();
             });
