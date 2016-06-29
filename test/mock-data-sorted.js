c89a2881f40e6db99aa450d2d0deb86a77af116a
Joel M <joel@mariachi.fiesta>
2016-06-29 00:15:59 -0700

    sort test and mock data added

---
+++ test/mock-data-sorted.js
@@ -0,0 +1,23 @@
+var data0 = {
+  results: [{
+    agony: 1.8
+  }, {
+    agony: 1.802
+  }, {
+    agony: 1.8009004502251125
+  }, {
+    agony: 4
+  }, {
+    agony: 12
+  }, {
+    agony: 14
+  }, {
+    agony: 17
+  }, {
+    agony: 100
+  }, {
+    agony: 100000000
+  }]
+};
+
+module.exports = data0;
---
+++ test/mock-data-unsorted.js
@@ -0,0 +1,31 @@
+var data1 = {
+  results: [{
+    agony: 1.8009004502251125
+  }, {
+    agony: 4
+  }, {
+    agony: 14
+  }]
+};
+
+var data2 = {
+  results: [{
+    agony: 17
+  }, {
+    agony: 1.8002
+  }, {
+    agony: 1.8
+  }]
+};
+
+var data3 = {
+  results: [{
+    agony: 12
+  }, {
+    agony: 100
+  }, {
+    agony: 100000000
+  }]
+};
+
+module.exports = [data1, data2, data3];
