653e5b32a8484e4fe5a2c4fb3165675dc36b332a
Joel M <joel@mariachi.fiesta>
2016-06-28 21:02:55 -0700

    console removed

--- common/scraper-promise.js
+++ common/scraper-promise.js
@@ -2,11 +2,10 @@
 
 var rp = require('request-promise');
 
-module.exports.getFlights = function (provider) {
+exports.getFlights = function (provider) {
   var options = {
     uri: 'http://localhost:9000/scrapers/' + provider,
     json: true
   };
-  console.log(provider);
   return rp(options);
 };
