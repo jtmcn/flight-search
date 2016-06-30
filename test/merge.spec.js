bb9c8d273f855c9db2fe85d53a1eaee480ddd5a1
Joel M <joel@mariachi.fiesta>
2016-06-30 15:33:13 -0700

    merge test finalized

--- test/merge.spec.js
+++ test/merge.spec.js
@@ -3,27 +3,35 @@
 var expect = require('chai').expect;
 var merger = require('../common/merger.js').mergeFlights;
 var unmerged = require('./mock-data-unmerged');
-var premerged = require('./mock-data-merged');
+var R = require('ramda');
 
 describe('Merged flight results', function () {
+  it('should be identical results sorted by Ramda', function () {
 
-  it('should be identical to pre-merged', function () {
-
-    var mergedResults = merger(unmerged);
-    var arraysAreIdentical = compareAgony(mergedResults.results, premerged.results);
+    let mergedResults = merger(unmerged);
+    let sortedWithLib = mergeAndSortWithRamda(unmerged);
+    let arraysAreIdentical = compareAgony(mergedResults.results, sortedWithLib.results);
     expect(arraysAreIdentical).to.be.true;
-
   });
 });
 
+function mergeAndSortWithRamda(resultsArray) {
+  let mergedArrays = [];
+  let sorted = {results: []};
+  resultsArray.map((arr) =>
+    arr.results.map((agonyObj) =>
+      mergedArrays.push(agonyObj)));
+  sorted.results = R.sortBy(R.prop('agony'), mergedArrays);
+  return sorted;
+}
+
 function compareAgony(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
+  if (arr1.length !== arr2.length)
     return false;
-  }
+
   for (var i = arr1.length; i--;) {
-    if (arr1[i].agony !== arr2[i].agony) {
+    if (arr1[i].agony !== arr2[i].agony)
       return false;
-    }
   }
   return true;
 }
