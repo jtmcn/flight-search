0e89c8c08274f889853ce5759efab1f0859e8c4f
Joel M <joel@mariachi.fiesta>
2016-06-30 15:54:21 -0700

    k-way merge success!

--- common/merger.js
+++ common/merger.js
@@ -1,19 +1,33 @@
-var R = require('ramda');
+'use strict';
 
 function mergeFlights(providerArray) {
-  let mergedArrays = [];
-  let sorted = {
-    results: []
-  };
+  let merged = {results: []};
 
-  providerArray.map(function (provider) {
-    provider.results.map(function (flights) {
-      mergedArrays.push(flights);
-    });
-  });
+  merged.results = providerArray
+    .map((provider) => provider.results)
+    .reduce(mergeSorted) // merge two arrays at time
+    .map((flightObj) => flightObj);
 
-  sorted.results = R.sortBy(R.prop('agony'), mergedArrays);
-  return sorted;
+  return merged;
+}
+
+function mergeSorted(a, b) {
+  let aLen = a.length, bLen = b.length,
+    i = 0, j = 0, k = 0,
+    mergedArr = new Array(aLen + bLen);
+
+  // as long as both provider arrays contain flight objects,
+  // compare next agony value in each and store flight of smaller in mergedArr
+  while (i < aLen && j < bLen)
+    mergedArr[k++] = a[i].agony < b[j].agony ? a[i++] : b[j++];
+
+  // if one array is empty store the rest of remaining array in mergedArr
+  while (i < aLen)
+    mergedArr[k++] = a[i++];
+  while (j < bLen)
+    mergedArr[k++] = b[j++];
+
+  return mergedArr;
 }
 
 exports.mergeFlights = mergeFlights;
